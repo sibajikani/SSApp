8ee6d488315f9065079dc6a17e2c6b0ed263ef6d
Sibabalwe Jikani <sibajikani@gmail.com>
2017-05-20 00:27:18 +0200

    More changes

--- app/src/main/java/com/example/s214092755/ssapp/Controllers/buy_list_adapter.java
+++ app/src/main/java/com/example/s214092755/ssapp/Controllers/buy_list_adapter.java
@@ -1,11 +1,10 @@
 package com.example.s214092755.ssapp.Controllers;
 
+import android.widget.ArrayAdapter;
+
 /**
  * Created by s214092755 on 2017/05/19.
  */
-/*
 
-public class buy_list_adapter extends  {
 
-}
-*/
+
